03973d44405bd3dbc38e8326dfb47bec713aa973
Lohith909 <lohithtallapudi909@gmail.com>
2026-08-05 19:58:59 +0530

    Added Prometheus metrics endpoint

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
 // Importing required packages
 const http = require('http');
 const express = require('express');
+const client = require('prom-client');
 
 const app = express();
 
-app.set('port', process.env.PORT || 3000); // Application port is set
-app.set('views', __dirname + '/app/server/views'); // Views folder is set
-app.set('view engine', 'ejs'); // View engine is set
-app.use(express.static(__dirname + '/app/public')); // Public folder containing static files is set
+// Collect default Node.js metrics
+client.collectDefaultMetrics();
 
-require('./app/routes')(app); // Routes are imported
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/app/server/views');
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/app/public'));
 
-http.createServer(app).listen(app.get('port'), function(){
-	console.log('The application is running on port ' + app.get('port'));
-}); // Http server is created
+require('./app/routes')(app);
+
+// Prometheus metrics endpoint
+app.get('/metrics', async (req, res) => {
+    res.set('Content-Type', client.register.contentType);
+    res.end(await client.register.metrics());
+});
+
+http.createServer(app).listen(app.get('port'), () => {
+    console.log(`The application is running on port ${app.get('port')}`);
+});
